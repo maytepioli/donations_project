4523634e630a55c7e83dfce59e852e75608113ff
Mayte Pioli <9423@holbertonstudents.com>
2025-02-10 21:35:01 -0300

    modified appbar

--- client/lib/views/donations/donations.dart
+++ client/lib/views/donations/donations.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
-// Definición del color personalizado
-const Color myColor =
-    Color(0xFFDEC3BE); // Puedes cambiar este color según tu preferencia
-
-// Pantalla principal
 class DonationsScreen extends StatefulWidget {
   const DonationsScreen({Key? key}) : super(key: key);
 
@@ -14,22 +9,12 @@ class DonationsScreen extends StatefulWidget {
 }
 
 class DonationsScreenState extends State<DonationsScreen> {
-  late Color myColor;
-  late Size mediaSize;
-  int _page = 1; // Inicializado en 1 para que se seleccione el botón del medio
-
-  final GlobalKey _bottomNavigationKey = GlobalKey();
+  int _page = 1; // Índice actual para la barra de navegación inferior
 
   @override
   Widget build(BuildContext context) {
-    mediaSize = MediaQuery.of(context).size;
-    myColor = const Color(0xFFDEC3BE);
-
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
         title: const Text(
           'Donaciones',
           style: TextStyle(
@@ -38,38 +23,25 @@ class DonationsScreenState extends State<DonationsScreen> {
             fontWeight: FontWeight.bold,
           ),
         ),
+        backgroundColor: Colors.white,
+        elevation: 0,
         centerTitle: true,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.notifications, color: Colors.black),
-            onPressed: () {
-              Navigator.pushNamed(context, '/notification');
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.account_circle, color: Colors.black),
-            onPressed: () {
-              Navigator.pushNamed(context, '/profile');
-            },
-          ),
-        ],
+        // Se han removido los íconos de notificaciones y perfil
       ),
       body: Padding(
-        padding: const EdgeInsets.all(
-            16.0), // Añadir padding para separar de los bordes
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             titleInput(TextEditingController()),
-            SizedBox(height: 24), // Aumentar el espacio entre los widgets
+            const SizedBox(height: 24),
             descriptionInput(TextEditingController()),
-            SizedBox(height: 24),
-            buildButtonContinue(context), // Botón añadido aquí
+            const SizedBox(height: 24),
+            buildButtonContinue(),
           ],
         ),
       ),
       bottomNavigationBar: CurvedNavigationBar(
-        key: _bottomNavigationKey,
         index: _page,
         height: 60.0,
         items: const <Widget>[
@@ -77,8 +49,8 @@ class DonationsScreenState extends State<DonationsScreen> {
           Icon(Icons.add, size: 30, color: Colors.black),
           Icon(Icons.map_outlined, size: 30, color: Colors.black),
         ],
-        color: myColor,
-        buttonBackgroundColor: myColor,
+        color: const Color(0xFFDEC3BE),
+        buttonBackgroundColor: const Color(0xFFDEC3BE),
         backgroundColor: Colors.white,
         animationCurve: Curves.easeInOut,
         animationDuration: const Duration(milliseconds: 300),
@@ -99,24 +71,24 @@ class DonationsScreenState extends State<DonationsScreen> {
   }
 }
 
-// Widget para el Título
 Widget titleInput(TextEditingController controller) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
-      Text(
+      const Text(
         'Título',
         style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold), // Aumentar el tamaño del texto
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+        ),
       ),
-      SizedBox(height: 10),
+      const SizedBox(height: 10),
       Container(
-        width: double.infinity, // Para que ocupe todo el ancho disponible
-        height: 60, // Aumentar la altura
+        width: double.infinity,
+        height: 60,
         decoration: BoxDecoration(
-          color: myColor,
-          borderRadius: BorderRadius.circular(16), // Bordes más redondeados
+          color: const Color(0xFFDEC3BE),
+          borderRadius: BorderRadius.circular(16),
         ),
         child: TextField(
           controller: controller,
@@ -124,12 +96,12 @@ Widget titleInput(TextEditingController controller) {
             hintText: 'Escribe el título aquí',
             border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(16),
-              borderSide: BorderSide.none, // Sin borde exterior
+              borderSide: BorderSide.none,
             ),
-            contentPadding: EdgeInsets.symmetric(
-                horizontal: 20, vertical: 18), // Más espacio interno
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
             filled: true,
-            fillColor: myColor,
+            fillColor: const Color(0xFFDEC3BE),
           ),
         ),
       ),
@@ -137,31 +109,31 @@ Widget titleInput(TextEditingController controller) {
   );
 }
 
-// Widget para la Descripción
 Widget descriptionInput(TextEditingController controller) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
-      Text(
+      const Text(
         'Descripción',
         style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold), // Aumentar el tamaño del texto
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+        ),
       ),
-      SizedBox(height: 10),
+      const SizedBox(height: 10),
       Container(
-        padding: EdgeInsets.all(16), // Más espacio interno
-        height: 150, // Aumentar la altura del contenedor
+        padding: const EdgeInsets.all(16),
+        height: 150,
         decoration: BoxDecoration(
           color: Colors.grey[200],
-          borderRadius: BorderRadius.circular(20), // Bordes más redondeados
+          borderRadius: BorderRadius.circular(20),
           border: Border.all(color: Colors.grey),
         ),
         child: TextField(
           controller: controller,
-          maxLines: null, // Permitir múltiples líneas
-          expands: true, // Expandir para llenar el contenedor
-          decoration: InputDecoration.collapsed(
+          maxLines: null,
+          expands: true,
+          decoration: const InputDecoration.collapsed(
             hintText: 'Agrega una descripción detallada aquí...',
           ),
         ),
@@ -170,24 +142,26 @@ Widget descriptionInput(TextEditingController controller) {
   );
 }
 
-// Botón de Continuar
-Widget buildButtonContinue(BuildContext context) {
-  return ElevatedButton(
-    onPressed: () {
-      Navigator.pushNamed(context, '/MapScreen');
-    },
-    style: ElevatedButton.styleFrom(
-      backgroundColor: myColor,
-      minimumSize: const Size(150, 50),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
+Widget buildButtonContinue() {
+  return Builder(
+    builder: (context) => ElevatedButton(
+      onPressed: () {
+        print("Botón Continuar presionado");
+        Navigator.pushNamed(context, '/MapScreen');
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color(0xFFDEC3BE),
+        minimumSize: const Size(150, 50),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
       ),
-    ),
-    child: const Text(
-      'Continuar',
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: 20,
+      child: const Text(
+        'Continuar',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+        ),
       ),
     ),
   );
