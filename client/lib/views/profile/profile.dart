0f8c69dbfd77c265aaaea5d87478a8088e011f08
Mayte Pioli <9423@holbertonstudents.com>
2025-02-11 11:28:23 -0300

    aesthetic profile modifications

--- client/lib/views/profile/profile.dart
+++ client/lib/views/profile/profile.dart
@@ -10,7 +10,8 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  // Si se obtiene la foto del usuario (por ejemplo, de Google), se asigna a esta variable; de lo contrario, se usa una imagen por defecto.
+  // Si se obtiene la foto del usuario (por ejemplo, de Google), se asigna a esta variable;
+  // de lo contrario, se usa una imagen por defecto.
   String profileImageUrl = '';
   final TextEditingController _nameController =
       TextEditingController(text: 'Juan Pérez');
@@ -32,7 +33,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
         backgroundColor: Colors.white,
         elevation: 0,
         centerTitle: true,
-        // Se muestra solo el botón de configuración.
         actions: [
           IconButton(
             icon: const Icon(Icons.settings, color: Colors.black),
@@ -43,81 +43,72 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ),
         ],
       ),
-      backgroundColor:
-          Colors.grey[200], // Fondo claro para resaltar las Cards "flotantes"
+      backgroundColor: Colors.grey[200],
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
+            // La foto de perfil permanece fuera del Card.
             _buildProfilePicture(),
             const SizedBox(height: 24),
+            // La tarjeta de donaciones también queda por fuera.
             _buildDonationCard(),
-            const SizedBox(height: 16),
-            _buildNameField(),
-            const SizedBox(height: 16),
-            _buildPhoneField(),
-            // Mayor espacio antes del botón "Guardar cambios"
-            const SizedBox(height: 40),
-            _buildSaveButton(),
+            const SizedBox(height: 24),
+            // Se agrupan los campos editables y el botón dentro de un Card.
+            Card(
+              elevation: 8,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    _buildNameField(),
+                    const SizedBox(height: 16),
+                    _buildPhoneField(),
+                    const SizedBox(height: 40),
+                    _buildSaveButton(),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 
+  // Widget para la foto de perfil.
   Widget _buildProfilePicture() {
-    return Stack(
-      children: [
-        CircleAvatar(
-          radius: 60,
-          backgroundImage: profileImageUrl.isNotEmpty
-              ? NetworkImage(profileImageUrl)
-              : const AssetImage('assets/images/default_profile.png')
-                  as ImageProvider,
-        ),
-        Positioned(
-          bottom: 0,
-          right: 0,
-          child: InkWell(
-            onTap: () {
-              // Lógica para cambiar la foto (por ejemplo, abrir la galería o usar la foto de Google)
-            },
-            child: Container(
-              padding: const EdgeInsets.all(8),
-              decoration: const BoxDecoration(
-                color: myColor,
-                shape: BoxShape.circle,
-              ),
-              child: const Icon(
-                Icons.camera_alt,
-                color: Colors.white,
-                size: 20,
-              ),
-            ),
-          ),
-        ),
-      ],
+    return CircleAvatar(
+      radius: 50,
+      backgroundColor: myColor,
+      child: profileImageUrl.isEmpty
+          ? const Icon(Icons.person, size: 50, color: Colors.white)
+          : ClipOval(child: Image.network(profileImageUrl, fit: BoxFit.cover)),
     );
   }
 
+  // Widget para la tarjeta de donaciones.
   Widget _buildDonationCard() {
     return Card(
+      color: myColor,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(12),
       ),
-      elevation: 4,
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             const Text(
               'Donaciones',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              style: TextStyle(color: Colors.white, fontSize: 18),
             ),
             Text(
-              donationsCount.toString(),
-              style: const TextStyle(fontSize: 18),
+              '$donationsCount',
+              style: const TextStyle(color: Colors.white, fontSize: 18),
             ),
           ],
         ),
@@ -125,61 +116,40 @@ class _ProfileScreenState extends State<ProfileScreen> {
     );
   }
 
+  // Widget para el campo del nombre.
   Widget _buildNameField() {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-      ),
-      elevation: 4,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        child: TextField(
-          controller: _nameController,
-          decoration: const InputDecoration(
-            border: InputBorder.none,
-            labelText: 'Nombre',
-          ),
-        ),
+    return TextFormField(
+      controller: _nameController,
+      decoration: const InputDecoration(
+        labelText: 'Nombre',
+        border: OutlineInputBorder(),
       ),
     );
   }
 
+  // Widget para el campo del teléfono.
   Widget _buildPhoneField() {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-      ),
-      elevation: 4,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        child: TextField(
-          controller: _phoneController,
-          keyboardType: TextInputType.phone,
-          decoration: const InputDecoration(
-            border: InputBorder.none,
-            labelText: 'Número de teléfono',
-          ),
-        ),
+    return TextFormField(
+      controller: _phoneController,
+      decoration: const InputDecoration(
+        labelText: 'Teléfono',
+        border: OutlineInputBorder(),
       ),
     );
   }
 
+  // Widget para el botón de guardar cambios.
   Widget _buildSaveButton() {
     return ElevatedButton(
       onPressed: () {
-        // Lógica para guardar los cambios.
+        // Aquí puedes agregar la lógica para guardar los cambios.
+        print('Guardar cambios');
       },
       style: ElevatedButton.styleFrom(
         backgroundColor: myColor,
-        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 12),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
+        minimumSize: const Size.fromHeight(50),
       ),
-      child: const Text(
-        'Guardar cambios',
-        style: TextStyle(fontSize: 16),
-      ),
+      child: const Text('Guardar cambios'),
     );
   }
 }
