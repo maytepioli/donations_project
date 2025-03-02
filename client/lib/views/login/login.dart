c3facd0766e82f42c17d2eca12e6f5bf0476b03b
ariel <ariel@gmail.com>
2025-03-02 11:52:07 -0300

    aaaaa

--- client/lib/views/login/login.dart
+++ client/lib/views/login/login.dart
@@ -1,55 +1,178 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+const Color myColor = Color(0xFF9D4EDD);
+
 class Login extends StatelessWidget {
-  const Login({super.key});
+  const Login({Key? key}) : super(key: key);
 
-  // Función simulada para iniciar sesión con Google y verificar registro
-  Future<void> signInWithGoogleAndNavigate(BuildContext context) async {
-    // Simula una demora de inicio de sesión (por ejemplo, 1 segundo)
-    await Future.delayed(const Duration(seconds: 1));
+  Future<void> signInWithGoogle(BuildContext context) async {
+    try {
+      GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      if (googleUser == null) return;
 
-    // Obtén SharedPreferences para revisar si el usuario ya está registrado.
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool isRegistered = prefs.getBool('isRegistered') ?? false;
+      GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
 
-    if (isRegistered) {
-      // Si ya está registrado, obtenemos también el rol (isCentro)
+      UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      bool isRegistered = prefs.getBool('isRegistered') ?? false;
       bool isCentro = prefs.getBool('isCentro') ?? false;
-      // Navega directamente al Home pasando el rol.
-      Navigator.pushReplacementNamed(context, '/home', arguments: isCentro);
-    } else {
-      // Si no está registrado, se navega a la página de selección de rol.
-      Navigator.pushReplacementNamed(context, '/rol');
+
+      if (isRegistered) {
+        Navigator.pushReplacementNamed(context, '/', arguments: isCentro);
+      } else {
+        Navigator.pushReplacementNamed(context, '/rol');
+      }
+    } catch (e) {
+      print("Error al iniciar sesión con Google: $e");
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFCC6D7F),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => signInWithGoogleAndNavigate(context),
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.white,
-            foregroundColor: Colors.black,
-            shape: const StadiumBorder(),
-            minimumSize: const Size(200, 60),
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              // Asegúrate de tener la imagen en assets/icons/image.png
-              Image.asset('assets/icons/image.png', width: 24, height: 24),
-              const SizedBox(width: 10),
-              const Text(
-                'Iniciar sesión con Google',
-                style: TextStyle(fontSize: 20),
+      backgroundColor: Color(0xFFF3F3F7),
+      body: Stack(
+        children: [
+          AnimatedPositionedWidget(top: -80, left: -80),
+          AnimatedPositionedWidget(top: 120, right: -90),
+          AnimatedPositionedWidget(bottom: -100, left: -50),
+          AnimatedPositionedWidget(bottom: 70, right: 70),
+          AnimatedPositionedWidget(top: -40, right: 40),
+          AnimatedPositionedWidget(bottom: 150, left: 150),
+          AnimatedPositionedWidget(top: 80, left: 250),
+          AnimatedPositionedWidget(bottom: 250, right: 200),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // Imagen agregada arriba del texto "HOPE BOX"
+                  Image.asset(
+                    'assets/fonts/logo.png',
+                    height: 100, // ajusta la altura según necesites
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'HOPE BOX',
+                    style: GoogleFonts.poppins(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: myColor,
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        side: BorderSide(color: myColor, width: 2),
+                      ),
+                      padding:
+                          EdgeInsets.symmetric(vertical: 14, horizontal: 20),
+                    ),
+                    onPressed: () => signInWithGoogle(context),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset(
+                          'assets/icons/image.png',
+                          height: 20,
+                        ),
+                        SizedBox(width: 8),
+                        Text(
+                          'Iniciar sesión con Google',
+                          style: TextStyle(fontSize: 16, color: myColor),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+        ],
+      ),
+    );
+  }
+}
+
+class AnimatedPositionedWidget extends StatefulWidget {
+  final double? top;
+  final double? left;
+  final double? right;
+  final double? bottom;
+
+  const AnimatedPositionedWidget(
+      {this.top, this.left, this.right, this.bottom});
+
+  @override
+  _AnimatedPositionedWidgetState createState() =>
+      _AnimatedPositionedWidgetState();
+}
+
+class _AnimatedPositionedWidgetState extends State<AnimatedPositionedWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3),
+    )..repeat(reverse: true);
+    _animation = Tween<double>(begin: -15, end: 15).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return Positioned(
+          top: widget.top != null ? widget.top! + _animation.value : null,
+          left: widget.left != null ? widget.left! + _animation.value : null,
+          right: widget.right != null ? widget.right! - _animation.value : null,
+          bottom:
+              widget.bottom != null ? widget.bottom! - _animation.value : null,
+          child: CircularDecoration(),
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+class CircularDecoration extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 120,
+      height: 120,
+      decoration: BoxDecoration(
+        color: myColor.withOpacity(0.3),
+        shape: BoxShape.circle,
       ),
     );
   }
